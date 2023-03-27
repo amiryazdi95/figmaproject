f7f385e891d94c3840d3cc8f75af085671b063e1
amiryazdi95 <104452584+amiryazdi95@users.noreply.github.com>
2023-03-27 16:54:08 +0330

    تراز/گزارش

--- testwork/code.js
+++ testwork/code.js
@@ -10,7 +10,7 @@ console.log(selections.textAlignHorizontal);
 }
 
 */
-
+/*
 (async () => {
     const text = figma.currentPage.selection[0]
  
@@ -20,6 +20,7 @@ console.log(selections.textAlignHorizontal);
     else if( text.textAlignHorizontal==="RIGHT")
     text.textAlignHorizontal="LEFT";
   })()
+  */
     /*
         const nodes = figma.currentPage.findAll();
         nodes.forEach(async (node) => {
@@ -111,7 +112,7 @@ function reverseChildren(node){
   }
    */
   
-
+/*
   function reverseChildren(node){
     const children = node.children;
     for (const child of children) {
@@ -153,3 +154,47 @@ function reverseChildren(node){
     }
    // figma.closePlugin('برعکس شد.');
   }
+  
+
+function reverseChildren(node){
+  const children = node.children;
+  for (const child of children) {
+    if(child.type==="TEXT")
+    {
+      
+      (async () => {
+        const text = child
+     
+        await figma.loadFontAsync(text.fontName)
+        if( text.textAlignHorizontal==="LEFT")
+        text.textAlignHorizontal="RIGHT";
+        else if( text.textAlignHorizontal==="RIGHT")
+        text.textAlignHorizontal="LEFT";
+      })()
+      
+      figma.notify(child.type);
+    }
+    if(child.layoutMode==="HORIZONTAL") { 
+    if(child.type === "GROUP" || child.type === "FRAME" || child.type === "COMPONENT" || child.type === "INSTANCE") {
+      reverseChildren(child); 
+    }
+   
+  }
+    const insertIndex = children.length - 1 - children.indexOf(child); 
+    node.insertChild(insertIndex, child); 
+  }
+  
+}
+//
+const selection = figma.currentPage.selection;
+if(selection.length == 0) {
+  figma.closePlugin('یک node انتخاب کنید.');
+} else {
+  for(const selections of selection)
+  {
+    const node = selections;
+    reverseChildren(node);
+  }
+ // figma.closePlugin('برعکس شد.');
+}
+*/
